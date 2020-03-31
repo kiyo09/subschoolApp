dc609e610f5969231ed2ba91fca75512a5ab5df2
kiyo09 <60495508+kiyo09@users.noreply.github.com>
2020-03-31 14:17:21 +0900

    レビュー機能サーバーサイド
    DB保存、viewに星表示確認

--- app/models/subschool.rb
+++ app/models/subschool.rb
@@ -1,3 +1,4 @@
 class Subschool < ApplicationRecord
   mount_uploader :image, ImageUploader
+  has_many :reviews
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'subschools#top'
-  resources :subschools, only: [:index, :show, :new, :create]
+  resources :subschools, only: [:index, :show, :new, :create] do
+    resources :reviews, only: :create    
+  end
 end
