92ad277a50c57d5508afbb5541635e86c21eab0d
kiyo09 <60495508+kiyo09@users.noreply.github.com>
2020-04-01 23:40:07 +0900

    ツイートにユーザー追加

--- db/migrate/20200331060842_create_tweets.rb
+++ db/migrate/20200331060842_create_tweets.rb
@@ -3,6 +3,7 @@ class CreateTweets < ActiveRecord::Migration[5.2]
     create_table :tweets do |t|
       t.text :tweet
       t.integer :subschool_id
+      t.integer :user_id
       t.timestamps
     end
   end
