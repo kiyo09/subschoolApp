1cd7e8d35193e4a924807d9e2a869e44dcb99f36
kiyo09 <60495508+kiyo09@users.noreply.github.com>
2020-04-05 00:01:37 +0900

    複数テーブル検索機能の実装
    検索フォーム作成
    複数テーブルの検索

--- app/models/subschool.rb
+++ app/models/subschool.rb
@@ -4,5 +4,13 @@ class Subschool < ApplicationRecord
   has_many :tweets
   has_many :stag_relations
   has_many :stags, through: :stag_relations
-  # accepts_nested_attributes_for :SubscTagRelations, allow_destroy: true
+  
+  def self.search(search)
+    return Subschool.all unless search
+    # Subschool.includes(:tweets).where(['name LIKE(?) OR tweets.tweet LIKE(?)' , "%#{search}%", "%#{search}%"]).references(:tweets)
+    # Subschool.where(['name LIKE(?) OR id LIKE(?)', "%#{search}%", "%#{search}%"])
+    Subschool.includes(:tweets, :stags, :reviews).where(['name LIKE(?) OR tweets.tweet LIKE(?) OR stags.tag LIKE(?) OR reviews.content LIKE(?)', "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%"]).references(:tweets, :reviews)
+    # Subschool.includes(:tweets, :reviews).where(['name LIKE(?) OR tweets.tweet LIKE(?) OR reviews.content LIKE(?)', "%#{search}%", "%#{search}%", "%#{search}%"]).references(:tweets, :reviews)
+  end
+
 end
