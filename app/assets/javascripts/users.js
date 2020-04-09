f598717c465d6a6cca3122ea2d2cb585c324906c
kiyo09 <60495508+kiyo09@users.noreply.github.com>
2020-04-09 11:40:48 +0900

    ログイン画面修正

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -19,3 +19,19 @@ $(function() {
       readURL(this);
   });
 });
+
+
+$(function() {
+  function readURL(input) {
+      if (input.files && input.files[0]) {
+      var reader = new FileReader();
+      reader.onload = function (e) {
+  $('#user_prev').attr('src', e.target.result);
+      }
+      reader.readAsDataURL(input.files[0]);
+      }
+  }
+  $("#user_image").change(function(){
+      readURL(this);
+  });
+});
